d65b462ba3b2aea80bbc42d02cf4782653bf5ab1
BalashovDV <korochun@yandex.ru>
2023-11-06 18:51:06 +0300

    Курс 5. Работа с базами данных. Домашнее задание 1. 1. create_tables.sql созданы запросы к БД для создания таблиц. 2. Написан скрипт для заполнения таблиц данными из файлов формата csv. Стадии: 2.1. Создание имен файлов с использованием модуля pathlib. 2.2. Чтение данных из файлов в списки словарей. 2.3. Для каждой таблицы формируются запросы для записи.

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,31 @@
 -- SQL-команды для создания таблиц
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(20) NOT NULL,
+    last_name varchar(20) NOT NULL
+    title varchar(50) NOT NULL,
+    birth_date date,
+    notes text
+);
+
+CREATE TABLE customers
+(
+    customer_id varchar(5) PRIMARY KEY,
+    company_name text,
+    contact_name varchar(25)
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(5) REFERENCES customers(customer_id) NOT NULL,
+    employee_id int REFERENCES employees(employee_id) NOT NULL,
+    order_date date,
+    ship_city varchar(20)
+);
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,78 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+from pathlib import Path
+import csv
+import psycopg2
+from psycopg2 import Error
+
+
+def main():
+	'''
+	Головная функция.
+	
+	1. Читаем данные из файлов csv каталога north_data
+	2. Создаем подключение к БД
+	3. Заносим прочитанные данные в базу данных
+	'''
+	
+	base_dir = Path(__file__).resolve().parent
+	data_dir = base_dir.joinpath('north_data')
+	
+	employees_data = data_dir.joinpath('employees_data.csv')
+	customers_data = data_dir.joinpath('customers_data.csv')
+	orders_data = data_dir.joinpath('orders_data.csv')
+	name_files = (employees_data, customers_data, orders_data)
+	
+	# Списки словарей из csv
+	employees_list = []
+	customers_list = []
+	orders_list = []
+	
+	# Формируем списки словарей с данными из csv
+	for i in range(3):
+		with open(name_files[i], encoding='utf-8') as r_file:
+			reader = csv.DictReader(r_file, delimiter=",")
+			for row in reader:
+				if i == 0:
+					employees_list.append(row)
+				elif i == 1:
+					customers_list.append(row)
+				else:
+					orders_list.append(row)
+	
+	name_tables = ('employees', 'customers', 'orders')
+	name_bd = 'north'
+	
+	# Формируем запросы к БД и записываем данные
+	try:
+		conn = psycopg2.connect(host='localhost', database=name_bd, user='postgres')
+		cur = conn.cursor()
+		for i in range(3):
+			del_statement = f'DELETE FROM {name_tables[i]};'
+			cur.execute(del_statement)
+			conn.commit()
+			if i == 0:
+				for employee in employees_list:
+					#"employee_id","first_name","last_name","title","birth_date","notes"
+					insert_statement = f'INSERT INTO {name_tables[i]} VALUES (%(employee_id)s, %(first_name)s, %(last_name)s, %(title)s, %(birth_date)s, %(notes)s)'
+					cur.execute(insert_statement, employee)
+			elif i == 1:
+				for customers in customers_list:
+					#"customer_id", "company_name", "contact_name"
+					insert_statement = f'INSERT INTO {name_tables[i]} VALUES (%(customer_id)s, %(company_name)s, %(contact_name)s)'
+					cur.execute(insert_statement, customers)
+			else:
+				for orders in orders_list:
+					#"order_id","customer_id","employee_id","order_date","ship_city"
+					insert_statement = f'INSERT INTO {name_tables[i]} VALUES (%(order_id)s, %(customer_id)s, %(employee_id)s, %(order_date)s, %(ship_city)s)'
+					cur.execute(insert_statement, orders)
+	except (Exception, Error) as error:
+		print("Не смогли подключиться", error)
+	finally:
+		if conn:
+			conn.commit()
+			cur.close()
+			conn.close()
+
+if __name__ == '__main__':
+	main()
