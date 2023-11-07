b3041deb1fb861c55e02592d44a3bf6c96afe5a1
BalashovDV <korochun@yandex.ru>
2023-11-07 06:19:21 +0300

    Курс 5. Работа с базами данных. Домашнее задание 1. 1. Вынесены константные значения в глобальную область: имена каталогов, имена файлов, названия таблиц и базы данных. 2. Вынесено отдельно чтение csv функцией. 3. Работа со списками для записи в БД переделана на список списков. 4. Требуется доработать запись в БД.

--- homework-1/main.py
+++ homework-1/main.py
@@ -5,6 +5,26 @@ import csv
 import psycopg2
 from psycopg2 import Error
 
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR.joinpath('north_data')
+
+EMPLOYEES_FILE = DATA_DIR.joinpath('employees_data.csv')
+CUSTOMERS_FILE = DATA_DIR.joinpath('customers_data.csv')
+ORDERS_FILE = DATA_DIR.joinpath('orders_data.csv')
+NAME_FILES = (EMPLOYEES_FILE, CUSTOMERS_FILE, ORDERS_FILE)
+
+NAME_BD = 'north'
+NAME_TABLES = ('employees', 'customers', 'orders')
+
+
+def read_csv(name_file: str):
+	list_csv = []
+	with open(name_file, encoding='utf-8') as r_file:
+		reader = csv.DictReader(r_file, delimiter=",")
+		for row in reader:
+			list_csv.append(row)
+	return list_csv
+
 
 def main():
 	'''
@@ -15,56 +35,36 @@ def main():
 	3. Заносим прочитанные данные в базу данных
 	'''
 	
-	base_dir = Path(__file__).resolve().parent
-	data_dir = base_dir.joinpath('north_data')
-	
-	employees_data = data_dir.joinpath('employees_data.csv')
-	customers_data = data_dir.joinpath('customers_data.csv')
-	orders_data = data_dir.joinpath('orders_data.csv')
-	name_files = (employees_data, customers_data, orders_data)
-	
 	# Списки словарей из csv
-	employees_list = []
-	customers_list = []
-	orders_list = []
+	# 'employees', 'customers', 'orders'
+	all_list = [[], [], []]
 	
 	# Формируем списки словарей с данными из csv
 	for i in range(3):
-		with open(name_files[i], encoding='utf-8') as r_file:
-			reader = csv.DictReader(r_file, delimiter=",")
-			for row in reader:
-				if i == 0:
-					employees_list.append(row)
-				elif i == 1:
-					customers_list.append(row)
-				else:
-					orders_list.append(row)
-	
-	name_tables = ('employees', 'customers', 'orders')
-	name_bd = 'north'
+		all_list[i] = read_csv(NAME_FILES[i])
 	
 	# Формируем запросы к БД и записываем данные
 	try:
-		conn = psycopg2.connect(host='localhost', database=name_bd, user='postgres')
+		conn = psycopg2.connect(host='localhost', database=NAME_BD, user='postgres')
 		cur = conn.cursor()
 		for i in range(3):
-			del_statement = f'DELETE FROM {name_tables[i]};'
+			del_statement = f'DELETE FROM {NAME_TABLES[2-i]};'
 			cur.execute(del_statement)
-			conn.commit()
+		for i in range(3):
 			if i == 0:
-				for employee in employees_list:
-					#"employee_id","first_name","last_name","title","birth_date","notes"
-					insert_statement = f'INSERT INTO {name_tables[i]} VALUES (%(employee_id)s, %(first_name)s, %(last_name)s, %(title)s, %(birth_date)s, %(notes)s)'
+				for employee in all_list[0]:
+					# "employee_id","first_name","last_name","title","birth_date","notes"
+					insert_statement = f'INSERT INTO {NAME_TABLES[i]} VALUES (%(employee_id)s, %(first_name)s, %(last_name)s, %(title)s, %(birth_date)s, %(notes)s)'
 					cur.execute(insert_statement, employee)
 			elif i == 1:
-				for customers in customers_list:
-					#"customer_id", "company_name", "contact_name"
-					insert_statement = f'INSERT INTO {name_tables[i]} VALUES (%(customer_id)s, %(company_name)s, %(contact_name)s)'
+				for customers in all_list[1]:
+					# "customer_id", "company_name", "contact_name"
+					insert_statement = f'INSERT INTO {NAME_TABLES[i]} VALUES (%(customer_id)s, %(company_name)s, %(contact_name)s)'
 					cur.execute(insert_statement, customers)
 			else:
-				for orders in orders_list:
-					#"order_id","customer_id","employee_id","order_date","ship_city"
-					insert_statement = f'INSERT INTO {name_tables[i]} VALUES (%(order_id)s, %(customer_id)s, %(employee_id)s, %(order_date)s, %(ship_city)s)'
+				for orders in all_list[2]:
+					# "order_id","customer_id","employee_id","order_date","ship_city"
+					insert_statement = f'INSERT INTO {NAME_TABLES[i]} VALUES (%(order_id)s, %(customer_id)s, %(employee_id)s, %(order_date)s, %(ship_city)s)'
 					cur.execute(insert_statement, orders)
 	except (Exception, Error) as error:
 		print("Не смогли подключиться", error)
@@ -73,6 +73,7 @@ def main():
 			conn.commit()
 			cur.close()
 			conn.close()
+
 
 if __name__ == '__main__':
 	main()
